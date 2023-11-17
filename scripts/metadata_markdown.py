42cffeb4e21eefe2c36b53a87e9038a1850600dd
Keigh Rim <keigh.rim@gmail.com>
2023-11-17 23:34:10 +0900

    updated migration scripts, mostly bug fixes

--- scripts/metadata_markdown.py
+++ scripts/metadata_markdown.py
@@ -32,85 +32,107 @@ submission_template = """
 
 frontmatter_template = """---
 layout: single
-title: "${name}:${app_version}"
+title: "${name} (${app_version})"
 ---"""
 
-def markdown_link(url):
-    return f"[{url}]({url})"
 
-markdown = io.StringIO()
-appmetadata = json.load(open(cwd / 'metadata.json'))
-markdown.write(string.Template(frontmatter_template).substitute(appmetadata))
-if not appmetadata['description']:
-    appmetadata['description'] = '(no description provided by the developer)'
-else:
-    appmetadata['description'] = re.sub(r'\n', '<br/>', appmetadata['description'])
-    
-if (cwd / 'submission.json').exists():
-    submitmetadata = json.load(open(cwd / 'submission.json'))
-    image_webpage = f'{appmetadata["url"]}/pkgs/container/{appmetadata["url"].rsplit("/",1)[-1]}/{appmetadata["app_version"]}'
-    markdown.write(string.Template(submission_template).substitute(**submitmetadata, image_webpage=image_webpage))
-markdown.write(string.Template(app_template).substitute(appmetadata))
+def convert_to_markdown(app_metadata, submission_metadata):
+    def markdown_link(url):
+        return f"[{url}]({url})"
 
-if 'analyzer_version' in appmetadata and appmetadata['analyzer_version']:
-    markdown.write(f"* Analyzer Version: {appmetadata['analyzer_version']}\n")
-if 'analyzer_license' in appmetadata and appmetadata['analyzer_license']:
-    markdown.write(f"* Analyzer License: {appmetadata['analyzer_license']}\n")
-if 'more' in appmetadata and appmetadata['more']:
-    markdown.write(f"* More Info: {appmetadata['more']}\n")
-    
-    
-def io_to_markdown(io_spec):
-    props = ', '.join(f'{k}={v}' for k, v in io_spec['properties'].items()) \
-        if 'properties' in io_spec and io_spec['properties'] \
-        else ""
-    return f"* {markdown_link(io_spec['@type'])} {'(required)' if 'required' in io_spec and io_spec['required'] else ''}\n###### {props if props else 'ANY'}\n"
-
-
-markdown.write('\n\n#### Inputs\n')
-for input_ in appmetadata['input']:
-    if isinstance(input_, list):
-        markdown.write('One of the following is required: [\n')
-        markdown.write(''.join(io_to_markdown(i) for i in input_))
-        markdown.write(']\n')
+    markdown = io.StringIO()
+    markdown.write(string.Template(frontmatter_template).substitute(app_metadata))
+    if not app_metadata['description']:
+        app_metadata['description'] = '(no description provided by the developer)'
     else:
-        markdown.write(io_to_markdown(input_))
+        app_metadata['description'] = re.sub(r'\n', '<br/>', app_metadata['description'])
+        
+    if submission_metadata:
+        image_webpage = f'{app_metadata["url"]}/pkgs/container/{app_metadata["url"].rsplit("/",1)[-1]}/{app_metadata["app_version"]}'
+        markdown.write(string.Template(submission_template).substitute(**submission_metadata, image_webpage=image_webpage))
+    markdown.write(string.Template(app_template).substitute(app_metadata))
 
-markdown.write('\n\n#### Configurable Parameters\n')
-markdown.write('###### Multivalued parameters can have two or more values.\n\n')
-
-if 'parameters' in appmetadata and appmetadata['parameters']:
-    markdown.write('|Name|Description|Type|Multivalued|Default|Choices|\n')
-    markdown.write('|----|-----------|----|-----------|-------|-------|\n')
-    for param in appmetadata['parameters']:
-        if 'default' in param:
-            if param['type'] == 'boolean':
-                if param['default'] == 0:
-                    default_value = 'false'
-                elif param['default'] in (True, False):
-                    default_value = str(param['default']).lower()
-                else:
-                    default_value = 'true'
-            else:
-                default_value = param['default']
-        else:
-            default_value = ''
-        if 'choices' in param:
-            cs = param['choices']
-        elif param['type'] == 'boolean':
-            cs = ['false', 'true']
-        else:
-            cs = []
-        choices = ', '.join(f'**_`{c}`_**' if c == default_value else f'`{c}`' for c in cs)
-        desc = re.sub(r'\n', '<br/>', param['description'])
-        markdown.write(f"|{param['name']}|{desc}|{param['type']}|{'Y' if param['multivalued'] else 'N'}|{default_value}|{choices}|\n")
-else:
-    markdown.write('##### N/A\n')
+    if 'analyzer_version' in app_metadata and app_metadata['analyzer_version']:
+        markdown.write(f"* Analyzer Version: {app_metadata['analyzer_version']}\n")
+    if 'analyzer_license' in app_metadata and app_metadata['analyzer_license']:
+        markdown.write(f"* Analyzer License: {app_metadata['analyzer_license']}\n")
+    if 'more' in app_metadata and app_metadata['more']:
+        markdown.write(f"* More Info: {app_metadata['more']}\n")
     
-markdown.write('\n\n#### Outputs\n')
-markdown.write('###### Note that not all output annotations are always generated.\n')
-for output in appmetadata['output']:
-    markdown.write(io_to_markdown(output))
+    
+    def io_to_markdown(io_spec):
+        props = ', '.join(f'{k}={v}' for k, v in io_spec['properties'].items()) \
+            if 'properties' in io_spec and io_spec['properties'] \
+            else ""
+        return f"* {markdown_link(io_spec['@type'])} {'(required)' if 'required' in io_spec and io_spec['required'] else ''}\n###### {props if props else 'ANY'}\n"
 
-out_f.write(markdown.getvalue())
-out_f.close()
+
+    markdown.write('\n\n#### Inputs\n')
+    for input_ in app_metadata['input']:
+        if isinstance(input_, list):
+            markdown.write('One of the following is required: [\n')
+            markdown.write(''.join(io_to_markdown(i) for i in input_))
+            markdown.write(']\n')
+        else:
+            markdown.write(io_to_markdown(input_))
+
+    markdown.write('\n\n#### Configurable Parameters\n')
+    markdown.write('###### Multivalued parameters can have two or more values.\n\n')
+
+    if 'parameters' in app_metadata and app_metadata['parameters']:
+        markdown.write('|Name|Description|Type|Multivalued|Default|Choices|\n')
+        markdown.write('|----|-----------|----|-----------|-------|-------|\n')
+        for param in app_metadata['parameters']:
+            if 'default' in param:
+                if param['type'] == 'boolean':
+                    if param['default'] == 0:
+                        default_value = 'false'
+                    elif param['default'] in (True, False):
+                        default_value = str(param['default']).lower()
+                    else:
+                        default_value = 'true'
+                else:
+                    default_value = param['default']
+            else:
+                default_value = ''
+            if 'choices' in param:
+                cs = param['choices']
+            elif param['type'] == 'boolean':
+                cs = ['false', 'true']
+            else:
+                cs = []
+            choices = ', '.join(f'**_`{c}`_**' if c == default_value else f'`{c}`' for c in cs)
+            desc = re.sub(r'\n', '<br/>', param['description'])
+            markdown.write(f"|{param['name']}|{desc}|{param['type']}|{'Y' if param['multivalued'] else 'N'}|{default_value}|{choices}|\n")
+    else:
+        markdown.write('##### N/A\n')
+    
+    markdown.write('\n\n#### Outputs\n')
+    markdown.write('###### Note that not all output annotations are always generated.\n')
+    for output in app_metadata['output']:
+        markdown.write(io_to_markdown(output))
+        
+    return markdown
+
+
+def main(directory_w_metadata=None):
+    if directory_w_metadata is not None:
+        cwd = Path(directory_w_metadata)
+        out_f = open(cwd / 'index.md', 'w')
+    else:
+        cwd = Path.cwd()
+        out_f = sys.stdout
+    app_metadata = json.load(open(cwd / 'metadata.json'))
+    submission_metadata = {}
+    if (cwd / 'submission.json').exists():
+        submission_metadata = json.load(open(cwd / 'submission.json'))
+    markdown = convert_to_markdown(app_metadata, submission_metadata)
+    out_f.write(markdown.getvalue())
+    out_f.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        main(None)
--- scripts/migration-230622.py
+++ scripts/migration-230622.py
@@ -2,51 +2,32 @@ import os
 import shutil
 from pathlib import Path
 import sys
+import metadata_markdown
 
 def migration(directory):
-    #iterate over the whole docs file
-    #find all the subdirectories contains anouther subdirectories which strats with v under the docs directory
-    #copy everything from there to the _apps directory, if '_apps' is not existing, create one
+    # iterate over the whole docs file
+    # find all the subdirectories contains another subdirectories which start with v 
+    # copy everything from there to the _apps directory, if '_apps' is not existing, create one
     for root, dirs, _ in os.walk(directory):
         for dir in dirs:
-            if '_site' in dirs:
-                dirs.remove('_site')
-            if '_apps' in dirs:
-                dirs.remove('_apps')
+            if dir.startswith('_'):
+                continue
             full_dir_path = Path(root) / dir
             if any(subdir.name.startswith('v') for subdir in full_dir_path.iterdir() if subdir.is_dir()):
                 destination_dir = os.path.join(directory, '_apps')
                 if not os.path.isdir(destination_dir):
                     os.makedirs(destination_dir)
                 destination_dir = Path(destination_dir) / full_dir_path.relative_to(directory)
-                shutil.copytree(full_dir_path, destination_dir)
+                shutil.move(full_dir_path, destination_dir)
         destination_dir = os.path.join(directory, '_apps')
-    #in the updated _apps folder, for those files end with .mds, add frontmatter looks like
-    # ---
-    # layout: single
-    # title:  "aapb-pua-kaldi-wrapper-v1"
-    # collection: apps
-    # permalink: aapb-pua-kaldi-wrapper/v1/
-    # ---
     for root, _, files in os.walk(destination_dir):
         for file in files:
-            if file.endswith('.md'):
-                file_path = os.path.join(root, file)
-                path = Path(file_path)
-                file_name = path.parts[-3]
-                version = path.parts[-2]
-                frontmatter = f"""---
-layout: single
-title: "{file_name}-{version}"
-collection: apps
-permalink: /{file_name}/{version}/
----"""
-                with open(file_path, 'r') as f:
-                    filedata = f.read()
-                filedata = frontmatter + filedata
-                with open(file_path, 'w') as f:
-                    f.write(filedata)
+            if file == 'metadata.json':
+                metadata_markdown.main(root)
 
 if __name__ == "__main__":
-    directory = Path(sys.argv[1])
+    if len(sys.argv) > 1:
+        directory = Path(sys.argv[1])
+    else:
+        directory = Path(__file__).parent.parent / 'docs'
     migration(directory)
