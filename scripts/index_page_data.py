cd16c99a8c8624e41bcc8e90e490a32231a1c4ca
Keigh Rim <keigh.rim@gmail.com>
2024-01-25 23:10:48 -0500

    more bug fixes in app-index insertion code

--- scripts/index_page_data.py
+++ scripts/index_page_data.py
@@ -19,23 +19,25 @@ shortid = app_id.rsplit('/', 1)[0]
 app_index_fname = 'docs/_data/app-index.json'
 apps_fname = 'docs/_data/apps.json'
 existing_index = json.load(open(app_index_fname))
+for v in existing_index.values():
+    v['latest_update'] = datetime.datetime.fromisoformat(v['latest_update'])
 existing_apps = json.load(open(apps_fname))
 
 if shortid not in existing_index:
     existing_index[shortid] = {
         'description': metadata['description'], 
-        'time': datetime.datetime.fromisoformat(submission['time']),
+        'latest_update': datetime.datetime.fromisoformat(submission['time']),
         'versions': [(app_version, submission['submitter'])]
     }
 if app_version not in existing_index[shortid]:
     existing_index[shortid]['versions'].insert(0, (app_version, submission['submitter']))
-    existing_index[shortid]['time'] = datetime.datetime.fromisoformat(submission['time'])
+    existing_index[shortid]['latest_update'] = datetime.datetime.fromisoformat(submission['time'])
     existing_apps.append(metadata)
     
-    for k, v in sorted(existing_index.items(), key=lambda item: item[1]['time'], reverse=True):
-        v['time'] = v['time'].isoformat()
-        v['versions'].sort(reverse=True)
-    sorted_existing_index = {k: v for k, v in sorted(existing_index.items(), key=lambda item: item[1]['time'], reverse=True)}
+    sorted_existing_index = {}
+    for k, v in sorted(existing_index.items(), key=lambda item: item[1]['latest_update'], reverse=True):
+        v['latest_update'] = v['latest_update'].isoformat()
+        sorted_existing_index[k] = v
 
     with open(app_index_fname, 'w') as f:
         json.dump(sorted_existing_index, f, indent=2)
