782a81d1b87875acf683bb6b515850c68866e06b
Keigh Rim <keigh.rim@gmail.com>
2025-03-03 21:22:14 -0500

    simplified app-pages generation, added app-page generation as a part of GHA

--- scripts/app_pages.py
+++ scripts/app_pages.py
@@ -1,39 +1,16 @@
 import os
 import json
 
-app_par_dir = os.path.join(os.path.dirname(__file__), '..', 'docs', '_apps')
+docs_dir = os.path.join(os.path.dirname(__file__), '..', 'docs')
 
-apps = [app for app in os.listdir(app_par_dir) if os.path.isdir(os.path.join(app_par_dir, app)) and not app.startswith('_')]
+apps = json.load(open(os.path.join(docs_dir, '_data', 'app-index.json')))
 
-for app in apps:
-    app_dir = os.path.join(app_par_dir, app)
-    app_vers = [app_ver for app_ver in os.listdir(app_dir) if os.path.isdir(os.path.join(app_dir, app_ver))]
-    app_submitters = dict()
-    app_descriptions = {app_ver: tuple() for app_ver in app_vers}
-    app_vers_sorted = list()
-    for app_ver in app_vers:
-        app_ver_dir = os.path.join(app_dir, app_ver)
-        json_files = [json_file for json_file in os.listdir(app_ver_dir) if json_file[-5:] == '.json']
-        for json_file in json_files:
-            json_file_path = os.path.join(app_ver_dir, json_file)
-            if json_file == 'metadata.json':
-                with open(json_file_path) as metadata_file:
-                    metadata = json.load(metadata_file)
-                    app_title = metadata['name']
-                    ver_num = metadata['app_version']
-                    app_descriptions[app_ver] = metadata['description']
-            elif json_file == 'submission.json':
-                with open(json_file_path) as submission_file:
-                    app_submission = json.load(submission_file)
-                    app_submitter = app_submission['submitter']
-                    submission_time = app_submission['time']
-                    app_submitters[app_ver] = app_submitter
-                    app_vers_sorted.append({'app_ver': app_ver, 'submission_time': submission_time})
+for app_uri, app_details in apps.items():
+    app_title = app_uri.rsplit('/', 1)[1]
+    app_submitters = {ver: submitter for ver, submitter in app_details['versions']}
+    app_description = app_details['description']
 
-    app_vers_sorted.sort(key=lambda x: x['submission_time'], reverse=True)
-    app_description = app_descriptions[app_vers_sorted[0]['app_ver']]
-
-    file_name = os.path.join(app_par_dir, app, 'index.md')
+    file_name = os.path.join(docs_dir, '_apps', app_title, 'index.md')
 
     f = open(file_name, "w")
 
@@ -44,11 +21,7 @@ for app in apps:
     f.write("---\n")
     f.write(f"{app_description}\n")
 
-    clams_url = "{{ url }}"
-
-    for app_ver in app_vers_sorted:
-        ver_num = app_ver['app_ver']
-        app_submitter = app_submitters[ver_num]
-        f.write(f"- [{ver_num}]({clams_url}/{app}/{ver_num}) ([`{app_submitter}`](https://github.com/{app_submitter}))\n")
+    for app_ver, app_submitter in app_submitters.items():
+        f.write(f"- [{app_ver}]({app_ver}) ([`@{app_submitter}`](https://github.com/{app_submitter}))\n")
 
     f.close()
